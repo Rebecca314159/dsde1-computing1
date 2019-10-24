562924df7bdc035f5ea04315a5af9bf7ad5e870e
Rebecca Allday <rebecca.allday19@imperial.ac.uk>
2019-10-24 15:07:53 +0100

    Fixing fuzzy

--- Week-4/fuzzy.py
+++ Week-4/fuzzy.py
@@ -6,27 +6,24 @@ Lint this file using PyLint.
 
 # This function does some maths on three numbers.
 def maths(input_a, input_b, input_c):
+    '''Function docstring: it does maths'''
     result = input_a * 3 - input_b + input_c
     return result
 
 # This function returns True or False.
 def choices(question):
-    if(question == True):
-        return True
-    else:
-        return False
-
+    '''Returns true or false'''
+    return bool(question)
 
 def main():
-    this_variable = 'useless'
-    
+    '''Does some maths'''
     # first function takes three numbers
-    answer = maths(3, 9 , 2.3)
+    answer = maths(3, 9, 2.3)
     print(answer)
 
     # second function takes a True or False
-    newAnswer = choices(True)
-    print(newAnswer)
+    new_answer = choices(True)
+    print(new_answer)
 
 if __name__ == '__main__':
     main()
